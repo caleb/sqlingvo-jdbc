fa8cdc7cc17f9289edb40329a60a33d153846072
Caleb Land <caleb@land.fm>
2016-06-11 17:44:21 -0400

    initial commit

---
+++ test/sqlingvo_jdbc/core_test.clj
@@ -0,0 +1,7 @@
+(ns sqlingvo-jdbc.core-test
+  (:require [clojure.test :refer :all]
+            [sqlingvo-jdbc.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
